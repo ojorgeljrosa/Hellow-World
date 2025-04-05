988c4b3027b4aed76fd5b8c79a30e55de1b5a47f
ojorgeljrosa <sileoagencia@gmail.com>
2025-04-05 14:53:52 -0300

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,19 @@
+/* Enquanto */
+
+var fatia = 3
+while (fatia>0) {
+    console.log(`Comendo fatia ${fatia}`)
+    fatia--
+}
+
+console.log("Fatias acabaram")
+
+
+estrutura de repetição com teste logico no inicio e no final 
+
+do {
+    console.log(`Comendo fatia ${fatia}`)
+    fatia--
+}
+while (fatia>0)                 
+console.log("Fatias acabaram")
