10f06fc32e4215132737dbd42589cf47da220f4d
ojorgeljrosa <sileoagencia@gmail.com>
2025-04-16 01:18:40 -0300

    adição de imagem

--- Calculador de Idade/script.js
+++ Calculador de Idade/script.js
@@ -1,12 +1,14 @@
 function calcular(){
     const anoNascimento = parseInt(window.document.getElementById('txtAnoNascimento').value)
-    const sexo = window.document.getElementById('radSexo').value
+    const sexo = window.document.getElementById('radMasculino').value
     const resultado = window.document.getElementById('divResultado')
-    const dataAtual = Date.getFullYear()
-    const idade = dataAtual - anoNascimento
+    const dataAtual = new Date();
+    const idade = dataAtual.getFullYear() - anoNascimento
+    let img = window.document.getElementById('img')
 
      if (sexo == "Masculino") {
         resultado.innerHTML = `Você tem ${idade} anos e é um homem`
+        img.src = 'https://img.freepik.com/fotos-premium/homem-adulto-consideravel-sorrindo_23-2148563511.jpg'
         
         } else if (idade >= 18) {
             resultado.innerHTML = `Você tem ${idade} anos e é uma mulher`
